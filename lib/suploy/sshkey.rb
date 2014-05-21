fd2bf43233b4be6c6c4ee5a5ca26bd830bb3e38b
flower-pot <flower-pot@flowerpot.io>
2014-05-21 19:51:12 +0200

    clean up sshkey module

--- lib/suploy/sshkey.rb
+++ lib/suploy/sshkey.rb
@@ -3,33 +3,30 @@ module Suploy
     def create(key_file)
       key_file ||= '~/.ssh/id_rsa.pub'
       key_file = File.expand_path(key_file)
-      if File.file? key_file
-        print "Adding key-file '#{key_file}' ... "
-        file = File.open(key_file, 'rb')
-        key_content = file.read
-        key_name = key_content.split.last
-        Suploy::Api::SshKey.create key_name, key_content
-        file.close
-        puts "Done"
-      else
+      if !(File.file? key_file)
         raise KeyFileDoesNotExist.new "Key-file '#{key_file}' does not exist."
       end
+      print "Adding key-file '#{key_file}' ... "
+      file = File.open(key_file, 'rb')
+      key_content = file.read
+      key_name = key_content.split.last
+      Suploy::Api::SshKey.create key_name, key_content
+      file.close
+      puts "Done"
     end
 
     def index
       ssh_keys = Suploy::Api::SshKey.index
       if ssh_keys.size > 0
-        table = Terminal::Table.new do |t|
-          t << ['Name', 'Fingerprint']
-          t.add_separator
-          ssh_keys.each do |k|
-            t << [k.info["title"], k.info["fingerprint"]]
-          end
-        end
-        puts table
-      else
-        puts "You have not added an SSH Key yet, why don't you do so..?"
+        raise NoSshKeysFound.new "You have not added an SSH Key yet, why don't you do so..?"
       end
+      table = Terminal::Table.new
+      table.title = "SSH Keys: #{ssh_keys.size}"
+      table.headings = ['Name', 'Fingerprint']
+      table.rows = ssh_keys.map do |k|
+        [k.info["title"], k.info["fingerprint"]]
+      end
+      puts table
     end
 
     def remove(key_name)
